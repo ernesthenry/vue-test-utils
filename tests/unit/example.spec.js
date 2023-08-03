80743f84af089282ca4ee854405f68b5a3da0548
ernesthenry <henry38ernest@gmail.com>
2023-08-03 11:13:02 +0300

    First test - assertion test

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,14 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mount } from '@vue/test-utils'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+
+const App = {
+  template: `
+  <div>Hello world </div>`
+}
+
+
+test('App', ()=>{
+  const wrapper=mount(App)
+  console.log(wrapper.html())
+  expect(wrapper.html()).toBe('<div>Hello world </div>')
 })
