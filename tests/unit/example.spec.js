243116e3dda5df84b2ab3deb6745308a935bbb21
ernesthenry <henry38ernest@gmail.com>
2023-08-03 15:28:49 +0300

    Refactor to SFC

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,34 +1,6 @@
 import { mount } from '@vue/test-utils'
-import { nextTick, ref } from 'vue'
+import App from './App.vue'
 
-
-const App = {
-
-  setup(){
-    const count = ref(0)
-
-    const increment = ()=>{
-      count.value+=1
-    }
-
-    return {
-      count,
-      increment
-    }
-  },
-
- 
-  template: `
-  <button @click='increment' />
-  <div v-if="count % 2 === 0">
-  Count:{{ count }}. Count is even
-  </div>
-
-  <div v-if="count % 2 !== 0">
-  Count:{{ count }}. Count is odd
-  </div>
-  `
-}
 
 function factory({data}= {data:{}}){
   return mount(App,{
